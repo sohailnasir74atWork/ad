d65ef3b9b352c0fc00049cb3827e13b7c94f7bb3
Ahmed Waqas <waqasnasir818@gmail.com>
2024-03-28 16:29:08 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
   const [retweetUrl, setRetweetUrl] = useState("");
   const [tgAnounc, setTGaANOUC] = useState("");
 
-  const [hasSubmitted, setHasSubmitted] = useState(false); 
-  const [referel, setReferel] = useState(0); 
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [referel, setReferel] = useState(0);
   const [score, setScore] = useState(0);
   const calculateScore = () => {
     let totalScore = 0;
@@ -42,43 +42,44 @@ function App() {
         fetchUserDetails(currentUser.uid);
       }
     });
-  
   }, [user]);
 
   const signInWithGoogle = () => {
-    signInWithPopup(auth, provider).then((result) => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
         const user = result.user;
         const queryParams = new URLSearchParams(window.location.search);
-        const referralCode = queryParams.get('referral');
+        const referralCode = queryParams.get("referral");
 
         if (referralCode) {
-            // Check if it's the user's first time logging in by checking their existence in the database
-            const newUserRef = ref(database, `users/${user.uid}`);
-            get(newUserRef).then((snapshot) => {
-                if (!snapshot.exists()) {
-                    // It's a new user, so increment the referrer's count
-                    incrementReferralCount(referralCode);
-                    
-                    // Set up new user's data here, including marking them as having signed up (if you track that)
-                }
-                // Additional logic for setting up or updating the user's own data can go here
-            });
+          // Check if it's the user's first time logging in by checking their existence in the database
+          const newUserRef = ref(database, `users/${user.uid}`);
+          get(newUserRef).then((snapshot) => {
+            if (!snapshot.exists()) {
+              // It's a new user, so increment the referrer's count
+              incrementReferralCount(referralCode);
+
+              // Set up new user's data here, including marking them as having signed up (if you track that)
+            }
+            // Additional logic for setting up or updating the user's own data can go here
+          });
         }
-    }).catch((error) => {
+      })
+      .catch((error) => {
         console.error("Error with Google sign-in:", error);
-    });
-};
+      });
+  };
 
-  
-
- 
   const incrementReferralCount = (referralCode) => {
     const referrerRef = ref(database, `users/${referralCode}/referralCount`);
     runTransaction(referrerRef, (currentCount) => {
       // If null, initialize to 0 before incrementing
       return currentCount + 1;
-    }).then(() => console.log("Referral count incremented"))
-      .catch((error) => console.error("Failed to increment referral count:", error));
+    })
+      .then(() => console.log("Referral count incremented"))
+      .catch((error) =>
+        console.error("Failed to increment referral count:", error)
+      );
   };
 
   const fetchUserDetails = (uid) => {
@@ -92,12 +93,13 @@ function App() {
         setFacebook(data.facebook || "");
         setReddit(data.reddit || "");
         setfacebookPost(data.facebookPost || "");
-        setScore(data.score || '')
-        setTGaANOUC(data.tgAnounc || '')
+        setScore(data.score || "");
+        setTGaANOUC(data.tgAnounc || "");
 
-         setHasSubmitted(data.hasSubmitted || false); // Set based on user data
+        setHasSubmitted(data.hasSubmitted || false); // Set based on user data
         if (data.referralCount) {
-              setReferel(data.referralCount)       }
+          setReferel(data.referralCount);
+        }
       } else {
         // New user setup
         update(userRef, {
@@ -124,7 +126,7 @@ function App() {
       facebookPost: facebookPost,
       tgAnounc: tgAnounc,
       hasSubmitted: true, // Update submission status
-      score:newScore
+      score: newScore,
     })
       .then(() => {
         setHasSubmitted(true);
@@ -156,114 +158,131 @@ function App() {
     <div className="App">
       <ToastContainer />
       <header className="App-header">
-        
-          {user ? (
-            <>
-              <h2>Welcome, {user.displayName}!</h2>
-              <p>Referel : {referel}</p>
-              <p>Account Score : {score}</p>
-              <form className="form" onSubmit={handleSubmit}>
-                <div className="label-container">
-                <span className="label-1">1. Follow Twitter: <a href="https://twitter.com/gqrapp">Click here</a></span>
-                <span className="label-2">Score: 1000</span></div>
-                <input
-                  type="text"
-                  placeholder="Twitter Handle"
-                  value={twitterHandle}
-                  onChange={(e) => setTwitterHandle(e.target.value)}
-                /><div className="label-container">
-                  <span className="label-1">2. Submit twitter tetweet link:</span><span className="label-2">Score: 1000</span></div>
-
-                <input
-                  type="text"
-                  placeholder="Retweet URL"
-                  value={retweetUrl}
-                  onChange={(e) => setRetweetUrl(e.target.value)}
-                />
-                <div className="label-container">
-                 <span className="label-1">3. Join Official Telegram Announcement Channel:<a href="https://t.me/genqr_app">Click here</a></span><span className="label-2">Score: 1000</span></div>
-
-                <input
-                  type="text"
-                  placeholder="@telegramHandler"
-                  value={telegramHandle}
-                  onChange={(e) => setTelegramHandle(e.target.value)}
-                />
-                <div className="label-container">
-                 <span className="label-1">4. Join Official Community Group:<a href="https://t.me/genqr_app">Click here</a></span><span className="label-2">Score: 1000</span></div>
-
-                <input
-                  type="text"
-                  placeholder="@telegramHandler"
-                  value={tgAnounc}
-                  onChange={(e) => setTGaANOUC(e.target.value)}
-                />               
-                  <div className="label-container">
-
-                <span className="label-1">5. Folow facebook page: [Provide the link to your Telegram group]</span><span className="label-2">Score: 1000</span></div>
-
-                <input
-                  type="text"
-                  placeholder="@facebookUserID"
-                  value={facebook}
-                  onChange={(e) => setFacebook(e.target.value)}
-                />                <div className="label-container">
-
-                <span className="label-1">6. Share facebook Pin Post</span><span className="label-2">Score: 1000</span></div>
-                <input
-                  type="text"
-                  placeholder="Share link URL"
-                  value={facebookPost}
-                  onChange={(e) => setfacebookPost(e.target.value)}
-                />
-                <div className="label-container">
-                <span className="label-1">7. Join Reddit</span><span className="label-2">Score: 1000</span></div>
-
-                <input
-                  type="text"
-                  placeholder="@redditUserName"
-                  value={reddit}
-                  onChange={(e) => setReddit(e.target.value)}
-                />
-                <br />
-                {!hasSubmitted ? ( // Show submit for new users or if editing
-                  <button
-                    type="submit"
-                    className={
-                      hasSubmitted
-                        ? "button button-update"
-                        : "button button-submit"
-                    }
-                  >
-                    {hasSubmitted ? "Update" : "Submit"}
-                  </button>
-                ) : (
-                  <button type="submit" className="button button-submit">
-                    Update
-                  </button>
-                )}
-              </form>
-              
-              <p>
-                Share your referral link:
-                <br />{" "}
-                <code>{`${window.location.origin}?referral=${user.uid}`}</code>
-              </p>
-              <button onClick={handleCopy} className="button submit">
-                Copy Link
-              </button>
+        {user ? (
+          <>
+            <h2>Welcome, {user.displayName}!</h2>
+            <p>Referel : {referel}</p>
+            <p>Account Score : {score}</p>
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="label-container">
+                <span className="label-1">
+                  1. Follow Twitter:{" "}
+                  <a href="https://twitter.com/gqrapp">Click here</a>
+                </span>
+                <span className="label-2">Score: 1000</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Twitter Handle"
+                value={twitterHandle}
+                onChange={(e) => setTwitterHandle(e.target.value)}
+              />
+              <div className="label-container">
+                <span className="label-1">2. Submit twitter tetweet link:</span>
+                <span className="label-2">Score: 1000</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Retweet URL"
+                value={retweetUrl}
+                onChange={(e) => setRetweetUrl(e.target.value)}
+              />
+              <div className="label-container">
+                <span className="label-1">
+                  3. Join Official Telegram Announcement Channel:
+                  <a href="https://t.me/genqr_app">Click here</a>
+                </span>
+                <span className="label-2">Score: 1000</span>
+              </div>
+              <input
+                type="text"
+                placeholder="@telegramHandler"
+                value={telegramHandle}
+                onChange={(e) => setTelegramHandle(e.target.value)}
+              />
+              <div className="label-container">
+                <span className="label-1">
+                  4. Join Official Community Group:
+                  <a href="https://t.me/genqr_app">Click here</a>
+                </span>
+                <span className="label-2">Score: 1000</span>
+              </div>
+              <input
+                type="text"
+                placeholder="@telegramHandler"
+                value={tgAnounc}
+                onChange={(e) => setTGaANOUC(e.target.value)}
+              />
+              <div className="label-container">
+                <span className="label-1">
+                  5. Folow facebook page: [Provide the link to your Telegram
+                  group]
+                </span>
+                <span className="label-2">Score: 1000</span>
+              </div>
+              <input
+                type="text"
+                placeholder="@facebookUserID"
+                value={facebook}
+                onChange={(e) => setFacebook(e.target.value)}
+              />{" "}
+              <div className="label-container">
+                <span className="label-1">6. Share facebook Pin Post</span>
+                <span className="label-2">Score: 1000</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Share link URL"
+                value={facebookPost}
+                onChange={(e) => setfacebookPost(e.target.value)}
+              />
+              <div className="label-container">
+                <span className="label-1">7. Join Reddit</span>
+                <span className="label-2">Score: 1000</span>
+              </div>
+              <input
+                type="text"
+                placeholder="@redditUserName"
+                value={reddit}
+                onChange={(e) => setReddit(e.target.value)}
+              />
               <br />
+              {!hasSubmitted ? ( // Show submit for new users or if editing
+                <button
+                  type="submit"
+                  className={
+                    hasSubmitted
+                      ? "button button-update"
+                      : "button button-submit"
+                  }
+                >
+                  {hasSubmitted ? "Update" : "Submit"}
+                </button>
+              ) : (
+                <button type="submit" className="button button-submit">
+                  Update
+                </button>
+              )}
+            </form>
 
-             
-            </>
-          ) : (
-            <div className="welcome">
-              <h1>Welcome to $GQR Official Airdrop Page</h1>
-              <button onClick={signInWithGoogle} className="button">
-                JOIN AIRDROP NOW
-              </button>
-            </div>
-          )}
+            <p>
+              Share your referral link:
+              <br />{" "}
+              <code>{`${window.location.origin}?referral=${user.uid}`}</code>
+            </p>
+            <button onClick={handleCopy} className="button submit">
+              Copy Link
+            </button>
+            <br />
+          </>
+        ) : (
+          <div className="welcome">
+            <h1>Welcome to $GQR Official Airdrop Page</h1>
+            <button onClick={signInWithGoogle} className="button">
+              JOIN AIRDROP NOW
+            </button>
+          </div>
+        )}
       </header>
     </div>
   );
